fceeb720eed11fd3ab7566e6584790f64f42602b
SUHONG\topic <topic828@Naver.com>
2024-08-17 07:42:01 +0900

    불필요한 호출 감소

--- src/shared/components/header/auth/index.tsx
+++ src/shared/components/header/auth/index.tsx
@@ -3,7 +3,6 @@ import UserMenu from './UserMenu';
 
 export default async function AuthButton() {
 	const session = await auth();
-	console.log(session?.user?.email);
 	if (!session?.user) return null;
 
 	return <UserMenu image={session.user.image!} />;
--- src/shared/hook/useAuthorized.ts
+++ src/shared/hook/useAuthorized.ts
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
 import { checkUser } from '@/api/user';
@@ -6,13 +6,21 @@ import { checkUser } from '@/api/user';
 export const useAuthorizedRouter = (path: string) => {
 	const { data: session } = useSession();
 	const router = useRouter();
+	const [userAuth, setUserAuth] = useState<number>();
 	useEffect(() => {
 		const authorize = async () => {
-			if (path !== '/' && session?.user?.email) {
+			if (!session?.user) {
+				setUserAuth(undefined);
+				return;
+			}
+
+			if (!userAuth && path !== '/' && session.user.email) {
 				try {
 					const user = await checkUser({ email: session.user.email });
 					if (!user || (user.auth !== 1 && user.auth !== 2 && user.auth !== 3)) {
 						router.push('/');
+					} else {
+						setUserAuth(user.auth);
 					}
 				} catch (error) {
 					console.error('Authorization check failed:', error);
